efb30d7d6069fa19b422786cd37a5b7688420d0e
SEDAT DURMUŞ <35039936+sedatdurmusss@users.noreply.github.com>
2022-05-11 10:37:05 +0300

    Update tamBolumveOrtalama.java
    https://app.patika.dev/courses/java101/pratik-cift-sayi-toplam

--- tamBolumveOrtalama.java
+++ tamBolumveOrtalama.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class tamBolumveOrtalama {
     public static void main(String[] args) {
-        int x;
+        int x, toplam = 0, sayac=0;
+        double ortalama;
 
         Scanner input = new Scanner(System.in);
 
@@ -12,8 +13,13 @@ public class tamBolumveOrtalama {
         for (int i =0; i<=x; i++){
             if ((i % 3 == 0) && ( i % 4 == 0)){
                 System.out.println(i);
+                toplam = toplam + i;
+                sayac++;
             }
         }
-
+        ortalama = toplam / sayac;
+        System.out.println("Toplam :" + toplam);
+        System.out.println("sayac :" + sayac);
+        System.out.println("3 ve 4'e Tam Bolunen Sayilarin Ortalamasi: " + ortalama);
     }
 }
